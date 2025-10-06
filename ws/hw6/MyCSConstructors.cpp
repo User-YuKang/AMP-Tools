5369ec8d668fbf98d01fcd76ffaf44fa9812ad90
Kong Yu Kang <yukang.kong.uni@gmail.com>
2025-10-06 08:29:21 -0600

    point agent cs constructor working

--- ws/hw6/MyCSConstructors.cpp
+++ ws/hw6/MyCSConstructors.cpp
@@ -4,34 +4,34 @@
 
 /* You can just move these classes to shared folder and include them instead of copying them to hw6 project*/
 
-std::pair<std::size_t, std::size_t> MyGridCSpace2D::getCellFromPoint(double x0, double x1) const {
-    // Implment your discretization procedure here, such that the point (x0, x1) lies within the returned cell
-    std::size_t cell_x = 0; // x index of cell
-    std::size_t cell_y = 0; // x index of cell
-    return {cell_x, cell_y};
-}
+// std::pair<std::size_t, std::size_t> MyGridCSpace2D::getCellFromPoint(double x0, double x1) const {
+//     // Implment your discretization procedure here, such that the point (x0, x1) lies within the returned cell
+//     std::size_t cell_x = 0; // x index of cell
+//     std::size_t cell_y = 0; // x index of cell
+//     return {cell_x, cell_y};
+// }
 
-// Override this method for computing all of the boolean collision values for each cell in the cspace
-std::unique_ptr<amp::GridCSpace2D> MyManipulatorCSConstructor::construct(const amp::LinkManipulator2D& manipulator, const amp::Environment2D& env) {
-    // Create an object of my custom cspace type (e.g. MyGridCSpace2D) and store it in a unique pointer. 
-    // Pass the constructor parameters to std::make_unique()
-    std::unique_ptr<MyGridCSpace2D> cspace_ptr = std::make_unique<MyGridCSpace2D>(m_cells_per_dim, m_cells_per_dim, env.x_min, env.x_max, env.y_min, env.y_max);
-    // In order to use the pointer as a regular GridCSpace2D object, we can just create a reference
-    MyGridCSpace2D& cspace = *cspace_ptr;
-    std::cout << "Constructing C-space for manipulator" << std::endl;
-    // Determine if each cell is in collision or not, and store the values the cspace. This `()` operator comes from DenseArray base class
-    cspace(1, 3) = true;
-    cspace(3, 3) = true;
-    cspace(0, 1) = true;
-    cspace(1, 0) = true;
-    cspace(2, 0) = true;
-    cspace(3, 0) = true;
-    cspace(4, 1) = true;
+// // Override this method for computing all of the boolean collision values for each cell in the cspace
+// std::unique_ptr<amp::GridCSpace2D> MyManipulatorCSConstructor::construct(const amp::LinkManipulator2D& manipulator, const amp::Environment2D& env) {
+//     // Create an object of my custom cspace type (e.g. MyGridCSpace2D) and store it in a unique pointer. 
+//     // Pass the constructor parameters to std::make_unique()
+//     std::unique_ptr<MyGridCSpace2D> cspace_ptr = std::make_unique<MyGridCSpace2D>(m_cells_per_dim, m_cells_per_dim, env.x_min, env.x_max, env.y_min, env.y_max);
+//     // In order to use the pointer as a regular GridCSpace2D object, we can just create a reference
+//     MyGridCSpace2D& cspace = *cspace_ptr;
+//     std::cout << "Constructing C-space for manipulator" << std::endl;
+//     // Determine if each cell is in collision or not, and store the values the cspace. This `()` operator comes from DenseArray base class
+//     cspace(1, 3) = true;
+//     cspace(3, 3) = true;
+//     cspace(0, 1) = true;
+//     cspace(1, 0) = true;
+//     cspace(2, 0) = true;
+//     cspace(3, 0) = true;
+//     cspace(4, 1) = true;
 
-    // Returning the object of type std::unique_ptr<MyGridCSpace2D> can automatically cast it to a polymorphic base-class pointer of type std::unique_ptr<amp::GridCSpace2D>.
-    // The reason why this works is not super important for our purposes, but if you are curious, look up polymorphism!
-    return cspace_ptr;
-}
+//     // Returning the object of type std::unique_ptr<MyGridCSpace2D> can automatically cast it to a polymorphic base-class pointer of type std::unique_ptr<amp::GridCSpace2D>.
+//     // The reason why this works is not super important for our purposes, but if you are curious, look up polymorphism!
+//     return cspace_ptr;
+// }
 
 //////////////////////////////////////////////////////////////
 
@@ -40,20 +40,26 @@ std::unique_ptr<amp::GridCSpace2D> MyPointAgentCSConstructor::construct(const am
     // Create an object of my custom cspace type (e.g. MyGridCSpace2D) and store it in a unique pointer. 
     // Pass the constructor parameters to std::make_unique()
     std::unique_ptr<MyGridCSpace2D> cspace_ptr = std::make_unique<MyGridCSpace2D>(m_cells_per_dim, m_cells_per_dim, env.x_min, env.x_max, env.y_min, env.y_max);
+    
     // In order to use the pointer as a regular GridCSpace2D object, we can just create a reference
     MyGridCSpace2D& cspace = *cspace_ptr;
+    double cell_width_height = 0.25;
+    check_collision_enviroument check_collision(env);
     std::cout << "Constructing C-space for point agent" << std::endl;
-    // Determine if each cell is in collision or not, and store the values the cspace. This `()` operator comes from DenseArray base class
-    cspace(0, 0) = true;
-    cspace(1, 1) = true;
-    cspace(2, 2) = true;
-    cspace(3, 3) = true;
-    cspace(4, 4) = true;
-    cspace(0, 4) = true;
-    cspace(1, 3) = true;
-    cspace(3, 1) = true;
-    cspace(4, 0) = true;
 
+    // Determine if each cell is in collision or not, and store the values the cspace. This `()` operator comes from DenseArray base class
+    for(int cell_n = 0; cell_n < m_cells_per_dim; cell_n++){
+        for(int cell_m = 0; cell_m < m_cells_per_dim; cell_m++){
+            for(int i = 0; i < 4; i++){
+                double random_x = env.x_min+cell_width_height*cell_n+(rand()%98+1)/100.0*cell_width_height;
+                double random_y = env.y_min+cell_width_height*cell_m+(rand()%98+1)/100.0*cell_width_height;
+                if (check_collision.all(Eigen::Vector2d(random_x, random_y))){
+                    cspace(cell_n, cell_m) = true;
+                    break;
+                }
+            }
+        }
+    }
     // Returning the object of type std::unique_ptr<MyGridCSpace2D> can automatically cast it to a polymorphic base-class pointer of type std::unique_ptr<amp::GridCSpace2D>.
     // The reason why this works is not super important for our purposes, but if you are curious, look up polymorphism!
     return cspace_ptr;
